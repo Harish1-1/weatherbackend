d69368e9998d8321b58448fcb23a38c33d14b186
Harish1-1 <109864814+Harish1-1@users.noreply.github.com>
2024-08-03 19:17:12 +0530

    Update sessions.js

--- routes/sessions.js
+++ routes/sessions.js
@@ -1,17 +1,9 @@
-/*const express = require('express');
-const { getSessions } = require('../controllers/sessionController');
-const router = express.Router();
-const authenticate = require('../middleware/authenticate');
 
-router.get('/', authenticate, getSessions);
-
-module.exports = router;
-*/
 
 const express = require('express');
-const { getSessions } = require('../controllers/sessionController'); // Ensure this import is correct
+const { getSessions } = require('../controllers/sessionController');
 const router = express.Router();
 
-router.get('/', getSessions); // Ensure getSessions is a valid function
+router.get('/', getSessions); 
 
 module.exports = router;
